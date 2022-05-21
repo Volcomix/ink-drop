358fe8df83f147bf7255b7d2bfa030ac371f00dc
Volcomix <Volcomix@users.noreply.github.com>
2022-05-21 19:58:59 +0200

    Bootstrap splat

--- src/main.ts
+++ src/main.ts
@@ -21,15 +21,38 @@ const arrays = {
 
 const bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays)
 
+const uniforms = {
+  u_time: 0,
+  u_resolution: [gl.canvas.width, gl.canvas.height],
+  u_mousePos: [0, 0],
+}
+
+function updateMousePos(event: MouseEvent) {
+  uniforms.u_mousePos = [event.offsetX, gl.canvas.height - event.offsetY]
+}
+
+gl.canvas.addEventListener('mousedown', (event) => {
+  updateMousePos(event)
+})
+
+gl.canvas.addEventListener('mousemove', (event) => {
+  if (event.buttons) {
+    updateMousePos(event)
+  }
+})
+
+gl.canvas.addEventListener('mouseup', () => {
+  uniforms.u_mousePos = [0, 0]
+})
+
 function animate(time: number) {
   stats.update()
 
   twgl.resizeCanvasToDisplaySize(gl.canvas)
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
 
-  const uniforms = {
-    a_time: time * 0.001,
-  }
+  uniforms.u_time = time * 0.001
+  uniforms.u_resolution = [gl.canvas.width, gl.canvas.height]
 
   gl.useProgram(programInfo.program)
   twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo)
--- src/simple.frag.ts
+++ src/simple.frag.ts
@@ -6,12 +6,16 @@ precision highp float;
 
 in vec2 v_texCoord;
 
-uniform float a_time;
+uniform float u_time;
+uniform vec2 u_resolution;
+uniform vec2 u_mousePos;
 
 out vec4 outColor;
 
 void main() {
-  outColor = vec4(v_texCoord, cos(a_time * 2.0) / 2.0 + 0.5, 1);
+  vec2 diff = v_texCoord * u_resolution - u_mousePos;
+  float quantity = exp(-dot(diff, diff) / 8192.0);
+  outColor = vec4(0, 0, quantity, 1);
 }
 
 `
