1d706c38985c9e5ab28ebee979b562acf2cc9eae
Volcomix <Volcomix@users.noreply.github.com>
2022-05-23 14:33:40 +0200

    Split main to multiple functions

--- src/main.ts
+++ src/main.ts
@@ -25,73 +25,85 @@ const buffer = twgl.createBufferInfoFromArrays(gl, arrays)
 const dye = createField()
 const velocity = createField()
 
+requestAnimationFrame(animate)
+
 function animate(time: number) {
   updateStats()
   twgl.resizeCanvasToDisplaySize(gl.canvas)
 
   if (mouse.isDown) {
-    const uniforms = {
-      u_time: time * 0.001,
-      u_resolution: [gl.canvas.width, gl.canvas.height],
-      u_mousePosition: mouse.position,
-      u_radius: config.splatRadius,
-    }
-
-    gl.useProgram(splatProgram.program)
-    twgl.setBuffersAndAttributes(gl, splatProgram, buffer)
-    twgl.setUniforms(splatProgram, uniforms)
-
-    dye.swap()
-
-    twgl.bindFramebufferInfo(gl, dye.next)
-
-    const dyeUniforms = {
-      u_color: config.dyeColor,
-      u_previousColor: dye.previous.attachments[0],
-    }
-
-    twgl.setUniforms(splatProgram, dyeUniforms)
-    twgl.drawBufferInfo(gl, buffer)
-
-    velocity.swap()
-
-    twgl.bindFramebufferInfo(gl, velocity.next)
-
-    const velocityUniforms = {
-      u_color: [...mouse.movement, 0],
-      u_previousColor: velocity.previous.attachments[0],
-    }
-
-    twgl.setUniforms(splatProgram, velocityUniforms)
-    twgl.drawBufferInfo(gl, buffer)
+    splat(time)
   }
 
   if (config.field === 'dye') {
-    twgl.bindFramebufferInfo(gl, null)
-
-    const uniforms = {
-      u_dye: dye.next.attachments[0],
-    }
-
-    gl.useProgram(dyeProgram.program)
-    twgl.setBuffersAndAttributes(gl, dyeProgram, buffer)
-    twgl.setUniforms(dyeProgram, uniforms)
-    twgl.drawBufferInfo(gl, buffer)
+    renderDye()
   } else {
-    twgl.bindFramebufferInfo(gl, null)
-
-    const uniforms = {
-      u_resolution: [gl.canvas.width, gl.canvas.height],
-      u_velocity: velocity.next.attachments[0],
-    }
-
-    gl.useProgram(velocityProgram.program)
-    twgl.setBuffersAndAttributes(gl, velocityProgram, buffer)
-    twgl.setUniforms(velocityProgram, uniforms)
-    twgl.drawBufferInfo(gl, buffer)
+    renderVelocity()
   }
 
   requestAnimationFrame(animate)
 }
 
-requestAnimationFrame(animate)
+function splat(time: number) {
+  const uniforms = {
+    u_time: time * 0.001,
+    u_resolution: [gl.canvas.width, gl.canvas.height],
+    u_mousePosition: mouse.position,
+    u_radius: config.splatRadius,
+  }
+
+  gl.useProgram(splatProgram.program)
+  twgl.setBuffersAndAttributes(gl, splatProgram, buffer)
+  twgl.setUniforms(splatProgram, uniforms)
+
+  dye.swap()
+
+  twgl.bindFramebufferInfo(gl, dye.next)
+
+  const dyeUniforms = {
+    u_color: config.dyeColor,
+    u_previousColor: dye.previous.attachments[0],
+  }
+
+  twgl.setUniforms(splatProgram, dyeUniforms)
+  twgl.drawBufferInfo(gl, buffer)
+
+  velocity.swap()
+
+  twgl.bindFramebufferInfo(gl, velocity.next)
+
+  const velocityUniforms = {
+    u_color: [...mouse.movement, 0],
+    u_previousColor: velocity.previous.attachments[0],
+  }
+
+  twgl.setUniforms(splatProgram, velocityUniforms)
+  twgl.drawBufferInfo(gl, buffer)
+}
+
+function renderDye() {
+  twgl.bindFramebufferInfo(gl, null)
+
+  const uniforms = {
+    u_dye: dye.next.attachments[0],
+  }
+
+  gl.useProgram(dyeProgram.program)
+  twgl.setBuffersAndAttributes(gl, dyeProgram, buffer)
+  twgl.setUniforms(dyeProgram, uniforms)
+  twgl.drawBufferInfo(gl, buffer)
+}
+
+function renderVelocity() {
+  twgl.bindFramebufferInfo(gl, null)
+
+  const uniforms = {
+    u_resolution: [gl.canvas.width, gl.canvas.height],
+    u_velocity: velocity.next.attachments[0],
+  }
+
+  gl.useProgram(velocityProgram.program)
+  twgl.setBuffersAndAttributes(gl, velocityProgram, buffer)
+  twgl.setUniforms(velocityProgram, uniforms)
+  twgl.drawBufferInfo(gl, buffer)
+}
--- src/mouse.ts
+++ src/mouse.ts
@@ -6,11 +6,6 @@ const mouse = {
   movement: [0, 0],
 }
 
-function updateMousePosition(event: MouseEvent) {
-  mouse.position = [event.offsetX, gl.canvas.height - event.offsetY]
-  mouse.movement = [event.movementX, -event.movementY]
-}
-
 gl.canvas.addEventListener('mousedown', (event) => {
   mouse.isDown = true
   updateMousePosition(event)
@@ -26,5 +21,10 @@ gl.canvas.addEventListener('mousemove', (event) => {
 gl.canvas.addEventListener('mouseup', () => {
   mouse.isDown = false
 })
+
+function updateMousePosition(event: MouseEvent) {
+  mouse.position = [event.offsetX, gl.canvas.height - event.offsetY]
+  mouse.movement = [event.movementX, -event.movementY]
+}
 
 export default mouse
