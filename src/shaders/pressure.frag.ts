5aceb4b7cf6d6523dce549feefcf82033e98dca5
Volcomix <Volcomix@users.noreply.github.com>
2022-05-26 17:23:04 +0200

    Refactor pressure rendering

--- src/shaders/pressure.frag.ts
+++ src/shaders/pressure.frag.ts
@@ -10,11 +10,14 @@ uniform sampler2D u_pressure;
 
 out vec4 outColor;
 
+const float maxLowPressure = 2.0;
+const float minHighPressure = 2.5;
+
 void main() {
   float pressure = texture(u_pressure, v_texCoord).r;
-  // TODO Refactor
-  vec3 lowPressure = vec3(0, 0.25, 1) * pressure * (1.0 - smoothstep(2.0, 2.5, pressure));
-  vec3 highPressure = vec3(1, 0.05, 0) * (pressure - 2.0) * smoothstep(2.0, 2.5, pressure);
+  float pressureTransition = smoothstep(2.0, 2.5, pressure);
+  vec3 lowPressure = vec3(0, 0.25, 1) * pressure * (1.0 - pressureTransition);
+  vec3 highPressure = vec3(1, 0.05, 0) * (pressure - maxLowPressure) * pressureTransition;
   outColor = vec4(lowPressure + highPressure, 1);
 }
 
