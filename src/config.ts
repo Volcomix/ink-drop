b55a86be42513b347dfa8c1fe7c894811e8a89f9
Volcomix <Volcomix@users.noreply.github.com>
2022-05-29 09:45:55 +0200

    Add button to reset the grid

--- src/config.ts
+++ src/config.ts
@@ -12,6 +12,10 @@ const config = {
   solverIterations: 50,
   pause: false,
   field: 'dye' as 'dye' | 'velocity' | 'pressure' | 'vorticity',
+  reset: () => {
+    gl.canvas.width = 0
+    gl.canvas.height = 0
+  },
 }
 
 if (config.stats) {
@@ -30,12 +34,10 @@ gui.addColor(config, 'dyeColor')
 gui.add(config, 'splatRadius', 8, 256, 1)
 gui.add(config, 'viscosity', 0, 0.005, 0.0001)
 gui.add(config, 'vorticity', 0, 10, 0.1)
-gui.add(config, 'gridResolution', 32, 2048, 1).onFinishChange(() => {
-  gl.canvas.width = 0
-  gl.canvas.height = 0
-})
+gui.add(config, 'gridResolution', 32, 2048, 1).onFinishChange(config.reset)
 gui.add(config, 'solverIterations', 1, 200, 1)
 gui.add(config, 'pause')
 gui.add(config, 'field', ['dye', 'velocity', 'pressure', 'vorticity'])
+gui.add(config, 'reset')
 
 export default config
